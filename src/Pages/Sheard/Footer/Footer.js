5eda043441278eeedbf9772f82327a39bc0bb72c
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-05-17 03:06:47 +0600

    Footer-complect

--- src/Pages/Sheard/Footer/Footer.js
+++ src/Pages/Sheard/Footer/Footer.js
@@ -1,7 +1,38 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-  return <div>HTIS IS FOOTER</div>;
+  const today = new Date();
+  const year = today.getFullYear();
+  return (
+    <footer className="footer-bg">
+      <div class="footer p-10  text-neutral ">
+        <div>
+          <span class="footer-title">Services</span>
+          <a class="link link-hover">Emergency Checkup</a>
+          <a class="link link-hover">Monthly Checkup</a>
+          <a class="link link-hover">Weekly Checkup</a>
+          <a class="link link-hover">Deep Checkup</a>
+        </div>
+        <div>
+          <span class="footer-title">ORAL HEALTH</span>
+          <a class="link link-hover">Fluoride Treatment</a>
+          <a class="link link-hover">Cavity Filling</a>
+          <a class="link link-hover">Teath Whitening </a>
+          <a class="link link-hover"></a>
+        </div>
+        <div>
+          <span class="footer-title">OUR ADDRESS</span>
+          <a class="link link-hover">New York - 101010 Hudson</a>
+          <a class="link link-hover">Privacy policy</a>
+          <a class="link link-hover">Cookie policy</a>
+        </div>
+      </div>
+      <div className="d-flex text-center mb-5">
+        <p>Copyright {year} All Rights Reserved</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
